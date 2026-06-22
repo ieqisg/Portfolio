80ba2e6c933dd51fe1ec3cdd4496afacaa1345f6
Marc Tamayo <marctamayo027@gmail.com>
2026-06-22 10:37:55 +0800

    Finished the contents and links of project cards

--- datas/ProjectsData.tsx
+++ datas/ProjectsData.tsx
@@ -3,7 +3,7 @@ export const projectsData = [
     title: "Fortis Nexus",
     description: "Developed a mentor–mentee matching system for thesis that applies a modified Gale–Shapley stable matching algorithm and weighted compatibility scoring. The system extracts technical keywords from mentor and mentee profiles and uses cosine similarity to measure research alignment, enabling stable and compatible mentor–mentee pairings.",
     stack: ["TypeScript", "Python", "Supabase", "NextJS", "NodeJS", "Express"],
-    content: "Content",
+    content: "Fortis Nexus is a web-based mentor–mentee matching system designed to support thesis mentorship by connecting students with mentors whose expertise aligns with their research needs. The system extracts and analyzes research interests and technical domains from mentor and mentee profiles, then uses cosine similarity to measure compatibility and a modified Gale–Shapley algorithm to generate stable mentor–mentee pairings. For example, a mentee conducting research in machine learning will be matched with a mentor who has expertise in machine learning and related areas, ensuring relevant guidance throughout the thesis development process.",
     websiteLink: "https://project-fs1om.vercel.app/",
     githubRepo: "https://github.com/ieqisg/fortis-nexus"
   },
@@ -11,13 +11,16 @@ export const projectsData = [
     title: "Expense Tracker",
     description: "Developed an expense tracking system that manages user income and expenses using full CRUD operations. The project serves as a learning exercise for backend fundamentals and is currently being refactored for better code optimization and structure.",
     stack: ["React", "Javascript", "MongoDB", "Supabase", "NodeJS", "Express"],
-    content: "Content",
+    content: "Expense Tracker is a web-based financial tracker that tracks monthly Income and expenses. This project serves as a training ground for CRUD operations, optimizing, refactoring and implementation of other technologies and frameworks that real systems use.",
+    website: "",
+    githubRepo: "https://github.com/ieqisg/expense-tracker"
   },
   {
     title: "Scheduler",
     description: "Built a web-based scheduler that scrapes course offerings from FEU Tech’s SOLAR system and generates all conflict-free schedule combinations ranked by user preferences, prioritizing fewer face-to-face days, fewer face-to-face sections, and fewer early morning classes.",
     stack: ["HTML", "Javascript", "NodeJS", "Express"],
-    content: "Content",
+    content: "",
+    githubRepo: "https://github.com/ieqisg/Scheduler"
   },
 
 ];
